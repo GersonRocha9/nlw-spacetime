45f884a2a43c96e2397f4581d429fb4837d4b32b
Gerson Rocha <gersonrocha9@gmail.com>
2023-05-18 09:54:29 -0300

    feature: auth on mobile app with expo auth session

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -1,25 +1,71 @@
+import * as SecureStore from 'expo-secure-store'
+
 import {
   Roboto_400Regular,
   Roboto_700Bold,
   useFonts,
 } from '@expo-google-fonts/roboto'
+import { makeRedirectUri, useAuthRequest } from 'expo-auth-session'
 import { ImageBackground, Text, TouchableOpacity, View } from 'react-native'
 
 import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
+import { useRouter } from 'expo-router'
 import { StatusBar } from 'expo-status-bar'
 import { styled } from 'nativewind'
-import blurBg from './src/assets/bg-blur.png'
-import NLWLogo from './src/assets/nlw-spacetime-logo.svg'
-import Stripes from './src/assets/stripes.svg'
+import { useEffect } from 'react'
+import blurBg from '../src/assets/bg-blur.png'
+import NLWLogo from '../src/assets/nlw-spacetime-logo.svg'
+import Stripes from '../src/assets/stripes.svg'
+import { api } from '../src/lib/api'
 
 const StyledStripes = styled(Stripes)
+const CLIENT_ID = '69a3e9ba3040ae7a192f'
+
+const discovery = {
+  authorizationEndpoint: 'https://github.com/login/oauth/authorize',
+  tokenEndpoint: 'https://github.com/login/oauth/access_token',
+  revocationEndpoint: `https://github.com/settings/connections/applications/${CLIENT_ID}`,
+}
 
 export default function App() {
+  const router = useRouter()
+
   const [hasLoadedFonts] = useFonts({
     Roboto_400Regular,
     Roboto_700Bold,
     BaiJamjuree_700Bold,
   })
+
+  const [request, response, signInWithGitHub] = useAuthRequest(
+    {
+      clientId: CLIENT_ID,
+      scopes: ['identity'],
+      redirectUri: makeRedirectUri({
+        scheme: 'nlwspacetime',
+      }),
+    },
+    discovery,
+  )
+
+  async function handleGitHubOAuthCode(code: string) {
+    const response = await api.post('/register', {
+      code,
+    })
+
+    const { token } = response.data
+
+    await SecureStore.setItemAsync('token', token)
+
+    router.push('/memories')
+  }
+
+  useEffect(() => {
+    if (response?.type === 'success') {
+      const { code } = response.params
+
+      handleGitHubOAuthCode(code)
+    }
+  }, [response])
 
   if (!hasLoadedFonts) {
     return null
@@ -42,7 +88,7 @@ export default function App() {
           </Text>
 
           <Text className="text-center font-body text-base leading-relaxed text-gray-100">
-            Colecione momentos marcantes da jua jornada e compartilhe (se
+            Colecione momentos marcantes da sua jornada e compartilhe (se
             quiser) com o mundo!
           </Text>
         </View>
@@ -50,6 +96,9 @@ export default function App() {
         <TouchableOpacity
           activeOpacity={0.7}
           className="rounded-full bg-green-500 px-5 py-2"
+          onPress={() => {
+            signInWithGitHub()
+          }}
         >
           <Text className="font-alt text-sm uppercase text-black">
             Cadastrar lembrança
